b6922e2d5804d3e9803b5e5ca870b4d3a60c4547
Apirak Panatkool <apirakb@gmail.com>
2023-11-09 14:06:47 +0700

    add member

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,6 @@
 class Team < ApplicationRecord
   validates :name, presence: true
+
+  has_many :members
+  has_many :users, through: :members
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_09_055514) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_09_064947) do
+  create_table "members", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "tenant_id", null: false
+    t.json "roles", default: {}, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tenant_id"], name: "index_members_on_tenant_id"
+    t.index ["user_id"], name: "index_members_on_user_id"
+  end
+
   create_table "teams", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -29,4 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_09_055514) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "members", "tenants"
+  add_foreign_key "members", "users"
 end
