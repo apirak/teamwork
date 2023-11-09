b1ef1821a07a599946b5a2e1b45e2eb4152b52ac
Apirak Panatkool <apirakb@gmail.com>
2023-11-09 22:24:16 +0700

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user1 =
+  User.create(
+    email: "user1@gmail.com",
+    password: "valid1",
+    password_confirmation: "valid1",
+  )
+
+user2 =
+  User.create(
+    email: "user2@gmail.com",
+    password: "valid1",
+    password_confirmation: "valid1",
+  )
+
+team = Team.new(name: "Team1")
+team.members.create(user: user1, roles: { admin: true })
+
+team = Team.new(name: "Team2")
+team.members.create(user: user1, roles: { admin: true })
+
+team = Team.new(name: "Team2")
+team.members.create(user: user2, roles: { admin: true })
+
+team = Team.new(name: "Team2")
+team.members.create(user: user2, roles: { admin: true })
